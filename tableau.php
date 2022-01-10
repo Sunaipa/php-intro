a8e98316860721699fb26a951ca26c604c179482
André Fernandes <andref.pro@gmail.com>
2022-01-10 16:33:01 +0100

    quelques tableaux

---
+++ tableau.php
@@ -0,0 +1,21 @@
+<?php 
+
+$cities = [
+    "Paris", "Berlin", "Madrid", "Dublin"
+];
+
+foreach($cities as $item) {
+    echo $item."<br>";
+}
+
+foreach($cities as $key => $item) {
+    echo $key." = ". $item."<br>";
+}
+// Choix aléatoire d'un ville
+echo $cities[array_rand($cities)];
+
+shuffle($cities);
+var_dump($cities)
+
+
+?>
