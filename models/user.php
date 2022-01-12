76df8e5e92eb4a24ca02956968e748a912fef216
André Fernandes <andref.pro@gmail.com>
2022-01-12 14:54:31 +0100

    comments

--- models/user.php
+++ models/user.php
@@ -2,12 +2,23 @@
 
 define("USER_PATH", 'data/users.json');
 
+/**
+ * Récupération d'une liste d'utilisateur sur un fichier Json
+ *
+ * @return array
+ */
 function getUserList(): array {
     $data = file_get_contents(USER_PATH, true);
     return json_decode($data, true);
 };
 
-
+/**
+ * Comparer saisie utilisateur avec une userList récupérer par : userList()
+ *
+ * @param string $login
+ * @param string $password
+ * @return boolean
+ */
 function authenticateUser (string $login, string $password): bool {
     $userList = getUserList();
     $user = array_filter($userList, function($item) use ($login, $password) {
